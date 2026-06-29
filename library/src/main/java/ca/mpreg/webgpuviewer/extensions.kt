097258f623fd88474c30ece1f66a879d8dd5ddd4
w <wvvwvvvvwvvw@gmail.com>
2026-06-29 14:46:13 -0700

    Update extensions.kt

--- library/src/main/java/ca/mpreg/webgpuviewer/extensions.kt
+++ library/src/main/java/ca/mpreg/webgpuviewer/extensions.kt
@@ -1,6 +1,5 @@
 package ca.mpreg.webgpuviewer
 
-import android.view.View
 import androidx.compose.foundation.gestures.calculatePan
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.input.pointer.AwaitPointerEventScope
@@ -8,16 +7,6 @@ import androidx.compose.ui.input.pointer.PointerEvent
 import androidx.compose.ui.input.pointer.PointerEventTimeoutCancellationException
 import androidx.compose.ui.input.pointer.PointerId
 import androidx.compose.ui.input.pointer.changedToUp
-
-fun View.scrollable(horizontal: Boolean): Boolean {
-    var p = parent
-    while (p != null && p is View) {
-        if (horizontal && (p.canScrollHorizontally(1) || p.canScrollHorizontally(-1))) return true
-        if (!horizontal && (p.canScrollVertically(1) || p.canScrollVertically(-1))) return true
-        p = p.parent
-    }
-    return false
-}
 
 suspend fun AwaitPointerEventScope.waitForCleanUp(
     pointerId: PointerId, timeout: Long, touchSlop: Float
